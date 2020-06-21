04f7da802c92681a2e56fd51abd1d58bb4dc80c0
pjy4183 <53017522+pjy4183@users.noreply.github.com>
2020-06-20 22:14:59 -0500

    Update main.dart

--- weatherwithu/lib/main.dart
+++ weatherwithu/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int temperature = 28;
+  int temperature = 30;
   String location = 'Las Vegas';
   @override
   Widget build(BuildContext context) {
