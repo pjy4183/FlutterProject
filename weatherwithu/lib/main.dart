7ac580b235cc3dd0c7f8afac7814ec376d372c08
pjy4183 <pjy4183@gmail.com>
2020-06-22 23:24:35 -0500

    Update

--- weatherwithu/lib/main.dart
+++ weatherwithu/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +12,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+<<<<<<< HEAD
   int temperature = 30;
   String location = 'Las Vegas';
+=======
+  int temperature = 0;
+  String location = 'San Fransisco';
+  int woeid = 2487956;
+  String status = 'clear';
+
+  String searchURL = 'https://www.metaweather.com/api/location/search/?query=';
+  String locationURL = 'https://www.metaweather.com/api/location/';
+
+  void fetchSearch( String input) async {
+    var searchResult = await http.get(searchURL + input);
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result['title'];
+      woeid = result['woeid'];
+    });
+  }
+
+  void fetchLocation() async {
+    var locationResult = await http.get(locationURL + woeid.toString());
+    var result = json.decode(locationResult.body);
+    var data = result['consolidated_weather'][0];
+
+    setState(() {
+      temperature = data['the_temp'].round();
+      status = data['weather_state_name'].replaceAll(' ', '').toLowerCase();
+    });
+    print(temperature);
+  }
+
+  void onTextFieldSubmitted(String input){
+    fetchSearch(input);
+    fetchLocation();
+  }
+
+>>>>>>> Getting API
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/clear.png'),
+            image: AssetImage('images/$status.png'),
             fit: BoxFit.cover,
           ),
         ),
@@ -49,6 +89,9 @@ class _MyAppState extends State<MyApp> {
                   Container(
                     width: 300,
                     child: TextField(
+                      onSubmitted: (String input){
+                        onTextFieldSubmitted(input);
+                      },
                       style: TextStyle(color: Colors.white, fontSize: 25),
                       decoration: InputDecoration(
                         hintText: 'Search location here...',
