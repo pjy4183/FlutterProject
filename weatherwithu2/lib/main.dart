5efe3925fbf40eccbd3902381fffe9d980c071c3
pjy4183 <pjy4183@gmail.com>
2020-06-29 22:40:07 -0500

    Update Clickable location icon

--- weatherwithu2/lib/main.dart
+++ weatherwithu2/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:weatherwithu2/weatherModel.dart';
 import 'package:weatherwithu2/weatherRepo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -14,114 +17,194 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        //visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.grey[900],
-        body: BlocProvider(
-          builder: (context) => WeatherBloc(WeatherRepo()),
-          child: SearchPage(),
-        )
-      )
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          //visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.grey[900],
+            body: BlocProvider(
+              builder: (context) => WeatherBloc(WeatherRepo()),
+              child: SearchPage(),
+            )));
   }
 }
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     final weatherBloc = BlocProvider.of<WeatherBloc>(context);
     var cityController = TextEditingController();
-  
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-
-
-        Center(
-            child: Container(
-              child: FlareActor("assets/WorldSpin.flr", fit: BoxFit.contain, animation: "roll",),
-              height: 300,
-              width: 300,
-            )
-        ),
-
-
-
-        BlocBuilder<WeatherBloc, WeatherState>(
-          builder: (context, state){
-            if(state is WeatherIsNotSearch)
-              return Container(
-                padding: EdgeInsets.only(left: 32, right: 32,),
-                child: Column(
+    String errorMessage = "";
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    Text("Search Weather", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500, color: Colors.white70),),
-                    Text("Instanly", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200, color: Colors.white70),),
-                    SizedBox(height: 24,),
-                    TextFormField(
-                      controller: cityController,
-
-                      decoration: InputDecoration(
-
-                        prefixIcon: Icon(Icons.search, color: Colors.white70,),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            borderSide: BorderSide(
-                                color: Colors.white70,
-                                style: BorderStyle.solid
-                            )
-                        ),
-
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            borderSide: BorderSide(
-                                color: Colors.blue,
-                                style: BorderStyle.solid
-                            )
-                        ),
-
-                        hintText: "City Name",
-                        hintStyle: TextStyle(color: Colors.white70),
-
-                      ),
-                      style: TextStyle(color: Colors.white70),
-
-                    ),
-
-                    SizedBox(height: 20,),
                     Container(
-                      width: double.infinity,
-                      height: 50,
-                      child: FlatButton(
-                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                        onPressed: (){
-                          weatherBloc.add(FetchWeather(cityController.text));
-                        },
-                        color: Colors.lightBlue,
-                        child: Text("Search", style: TextStyle(color: Colors.white70, fontSize: 16),),
-
+                        margin: const EdgeInsets.all(13),
+                        height: 45,
+                        width: 330,
+                        child: TextField(
+                          onSubmitted: (String input) {
+                            weatherBloc.add(FetchWeather(input));
+                          },
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                          decoration: InputDecoration(
+                            hintText: 'Search location here....',
+                            hintStyle:
+                                TextStyle(color: Colors.white, fontSize: 18),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.white70,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(
+                                    color: Colors.white70,
+                                    style: BorderStyle.solid)),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(
+                                    color: Colors.blue,
+                                    style: BorderStyle.solid)),
+                          ),
+                        )),
+                    Text(
+                      errorMessage,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 15.0,
                       ),
                     )
-
                   ],
                 ),
-              );
-            else if(state is WeatherIsLoading)
-              return Center(child : CircularProgressIndicator());
-            else if(state is WeatherIsLoaded)
-              return ShowWeather(state.getWeather, cityController.text);
-            else
-              return Text("Error",style: TextStyle(color: Colors.white),);
-          },
-        )
-
-      ],
+                IconButton(
+                  icon: Icon(Icons.location_on),
+                  color: Colors.white70,
+                  iconSize: 38,
+                  onPressed: () {
+                    _getCurrentLocation();
+                  },
+                ),
+              ]),
+          Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'WeatherWithU 2',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.blue),
+                ),
+                Container(
+                  child: FlareActor(
+                    "assets/WorldSpin.flr",
+                    fit: BoxFit.contain,
+                    animation: "roll",
+                  ),
+                  height: 300,
+                  width: 300,
+                )
+              ],
+            ),
+          ),
+          BlocBuilder<WeatherBloc, WeatherState>(
+            builder: (context, state) {
+              if (state is WeatherIsNotSearch)
+                return Container(
+                  padding: EdgeInsets.only(
+                    left: 32,
+                    right: 32,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Search Weather",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white70),
+                      ),
+                      Text(
+                        "Instanly",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w200,
+                            color: Colors.white70),
+                      ),
+                    ],
+                  ),
+                );
+              else if (state is WeatherIsLoading)
+                return Center(child: CircularProgressIndicator());
+              else if (state is WeatherIsLoaded)
+                return ShowWeather(state.getWeather, cityController.text);
+              else
+                return Text(
+                  "Error",
+                  style: TextStyle(color: Colors.white),
+                );
+            },
+          )
+        ],
+      ),
     );
+  }
+
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+  Position _currentPosition;
+  String _currentAddress;
+  _getCurrentLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+
+      _getAddressFromLatLng();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      final weatherBloc = BlocProvider.of<WeatherBloc>(context);
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = p[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+      weatherBloc.add(FetchWeather(place.locality));
+    } catch (e) {
+      print(e);
+    }
   }
 }
 
@@ -134,52 +217,76 @@ class ShowWeather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: EdgeInsets.only(right: 32, left: 32, top: 10),
-          child: Column(
-            children: <Widget>[
-              Text(weather.name+'/'+weather.country.toString(),style: TextStyle(color: Colors.white70, fontSize: 30, fontWeight: FontWeight.bold),),
-              SizedBox(height: 10,),
-
-              Text(weather.temp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 50),),
-              Text("Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Text(weather.temp_min.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
-                      Text("Min Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(weather.temp_max.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 30),),
-                      Text("Max Temprature",style: TextStyle(color: Colors.white70, fontSize: 14),),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: FlatButton(
-                  shape: new RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                  onPressed: (){
-                    BlocProvider.of<WeatherBloc>(context).add(ResetWeather());
-                  },
-                  color: Colors.lightBlue,
-                  child: Text("Search", style: TextStyle(color: Colors.white70, fontSize: 16),),
-
+        padding: EdgeInsets.only(right: 32, left: 32, top: 10),
+        child: Column(
+          children: <Widget>[
+            Text(
+              weather.name + '/' + weather.country.toString(),
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              weather.temp.round().toString() + "C",
+              style: TextStyle(color: Colors.white70, fontSize: 50),
+            ),
+            Text(
+              "Temprature",
+              style: TextStyle(color: Colors.white70, fontSize: 14),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      weather.temp_min.round().toString() + "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Min Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          )
-      );
+                Column(
+                  children: <Widget>[
+                    Text(
+                      weather.temp_max.round().toString() + "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Max Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: FlatButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () {
+                  BlocProvider.of<WeatherBloc>(context).add(ResetWeather());
+                },
+                color: Colors.lightBlue,
+                child: Text(
+                  "Search",
+                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
-
