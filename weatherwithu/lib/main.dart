9ab93ee23443cbd04d024d45dfd5145f33257a4a
pjy4183 <pjy4183@gmail.com>
2020-06-30 23:04:25 -0500

    Changing flare

--- weatherwithu/lib/main.dart
+++ weatherwithu/lib/main.dart
@@ -17,54 +17,56 @@ class _MyAppState extends State<MyApp> {
   int temperature;
   String location = 'San Fransisco';
   int woeid = 2487956;
-  String status = 'clear';
-  String icon = '';
-  String errorMessage ='';
+  String weather = 'clear';
+  String abbrevation = '';
+  String errorMessage = '';
 
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
   Position _currentPosition;
   String _currentAddress;
 
-  String searchURL = 'https://www.metaweather.com/api/location/search/?query=';
-  String locationURL = 'https://www.metaweather.com/api/location/';
+  String searchApiUrl =
+      'https://www.metaweather.com/api/location/search/?query=';
+  String locationApiUrl = 'https://www.metaweather.com/api/location/';
 
   initState() {
     super.initState();
     fetchLocation();
   }
 
-  void fetchSearch( String input) async {
-    try{
-      var searchResult = await http.get(searchURL + input);
+  void fetchSearch(String input) async {
+    try {
+      var searchResult = await http.get(searchApiUrl + input);
       var result = json.decode(searchResult.body)[0];
 
       setState(() {
-        location = result['title'];
-        woeid = result['woeid'];
-        errorMessage ='';
+        location = result["title"];
+        woeid = result["woeid"];
+        errorMessage = '';
       });
-    }
-    catch(error){
+    } catch (error) {
       setState(() {
-        errorMessage = "Sorry, cannot find the city...";
+        errorMessage =
+            "Sorry, we don't have data about this city. Try another one.";
       });
     }
   }
 
   void fetchLocation() async {
-    var locationResult = await http.get(locationURL + woeid.toString());
+    var locationResult = await http.get(locationApiUrl + woeid.toString());
     var result = json.decode(locationResult.body);
-    var data = result['consolidated_weather'][0];
+    var consolidated_weather = result["consolidated_weather"];
+    var data = consolidated_weather[0];
 
     setState(() {
-      temperature = data['the_temp'].round();
-      status = data['weather_state_name'].replaceAll(' ', '').toLowerCase();
-      icon = data['weather_state_abbr'];
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+      abbrevation = data["weather_state_abbr"];
     });
   }
 
-  void onTextFieldSubmitted(String input) async{
+  void onTextFieldSubmitted(String input) async {
     await fetchSearch(input);
     await fetchLocation();
   }
@@ -105,81 +107,96 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/$status.png'),
-            fit: BoxFit.cover,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/$weather.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: temperature==null ? Center(child: CircularProgressIndicator()): Scaffold(
-          appBar: AppBar(actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {_getCurrentLocation();},
-                child: Icon(Icons.location_city, size:36.0)
-              ),
-            )
-          ],
-          backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
-          backgroundColor: Colors.transparent,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Center(
-                    child: Image.network(
-                      'https://www.metaweather.com/static/img/weather/png/' +icon +'.png',
-                      width: 100,
-                    ),
+          child: temperature == null
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                  appBar: AppBar(
+                    actions: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            _getCurrentLocation();
+                          },
+                          child: Icon(Icons.location_city, size: 36.0),
+                        ),
+                      )
+                    ],
+                    backgroundColor: Colors.transparent,
+                    elevation: 0.0,
                   ),
-                  Center(
-                    child: Text(
-                      temperature.toString() + '°C',
-                      style: TextStyle(color: Colors.white, fontSize: 60),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      location,
-                      style: TextStyle(color: Colors.white, fontSize: 40),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    width: 300,
-                    child: TextField(
-                      onSubmitted: (String input){
-                        onTextFieldSubmitted(input);
-                      },
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                      decoration: InputDecoration(
-                        hintText: 'Search location here....',
-                        hintStyle: TextStyle(color: Colors.white, fontSize: 18),
-                        prefixIcon: Icon(Icons.search, color: Colors.white), 
+                  resizeToAvoidBottomInset: false,
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Center(
+                            child: Image.network(
+                              'https://www.metaweather.com/static/img/weather/png/' +
+                                  abbrevation +
+                                  '.png',
+                              width: 100,
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              temperature.toString() + ' °C',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 60.0),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              location,
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 40.0),
+                            ),
+                          ),
+                        ],
                       ),
-                    )
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 300,
+                            child: TextField(
+                              onSubmitted: (String input) {
+                                onTextFieldSubmitted(input);
+                              },
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                              decoration: InputDecoration(
+                                hintText: 'Search another location...',
+                                hintStyle: TextStyle(
+                                    color: Colors.white, fontSize: 18.0),
+                                prefixIcon:
+                                    Icon(Icons.search, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 32.0, left: 32.0),
+                            child: Text(errorMessage,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize:
+                                        Platform.isAndroid ? 15.0 : 20.0)),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  Text(
-                    errorMessage,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.redAccent, fontSize: Platform.isAndroid ? 15.0: 20.0
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+                )),
     );
   }
 }
